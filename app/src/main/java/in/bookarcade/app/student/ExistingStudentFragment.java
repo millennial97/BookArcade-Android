3ed773458feffb010010c64cb80a819243f598f9
ajays97 <ajaysrinivas530@gmail.com>
2018-09-27 22:20:14 +0530

    Designing WalletActivity.

---
+++ app/src/main/java/in/bookarcade/app/student/ExistingStudentFragment.java
@@ -0,0 +1,84 @@
+package in.bookarcade.app.student;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import in.bookarcade.app.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ExistingStudentFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ExistingStudentFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ExistingStudentFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public ExistingStudentFragment() {
+
+    }
+
+    public static ExistingStudentFragment newInstance() {
+        return new ExistingStudentFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_new_student, container, false);
+
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
